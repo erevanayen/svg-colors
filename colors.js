c4399469e7a25446b6bf4f3e33969d95bf0a5675
erevanayen <zed.durotan@gmail.com>
2023-08-07 17:52:52 +0200

    feat(script): all color combos
    now generating all possible color combinations

--- colors.js
+++ colors.js
@@ -1,8 +1,9 @@
 const availableColors = ["#000000", "#fb00ff", "#fff900"];
+const shirtColor = "#444444";
 
 const svgMaster = document.getElementById("svg-master");
 const generateButton = document.getElementById("but-generate");
-generateButton.addEventListener("click", changeColor);
+const results = document.getElementById("results");
 
 // get all paths from the svg
 const paths = svgMaster.querySelectorAll("path");
@@ -12,16 +13,51 @@ const pathsWithFill = Array.from(paths).filter(
 );
 const pathsWithOutline = Array.from(paths).filter((path) => path.style.stroke);
 
-// go through all paths with fill and change the color
-// todo: make sure that the same color is not used twice
-function changeColor() {
-  pathsWithFill.forEach((path) => {
-    const index = Math.floor(Math.random() * availableColors.length);
-    path.style.fill = availableColors[index];
-  });
+function generateColorVariations(colors, length) {
+  const variations = [];
 
-  pathsWithOutline.forEach((path) => {
-    const index = Math.floor(Math.random() * availableColors.length);
-    path.style.stroke = availableColors[index];
-  });
+  function generateVariations(currentVariation, remainingLength) {
+    if (remainingLength === 0) {
+      variations.push(currentVariation);
+      return;
+    }
+
+    for (const color of colors) {
+      generateVariations(currentVariation.concat(color), remainingLength - 1);
+    }
+  }
+
+  generateVariations([], length);
+  return variations;
 }
+
+function makeVariations() {
+  const colorVariations = generateColorVariations(
+    availableColors,
+    paths.length
+  );
+
+  // create a new svg for each variation
+  for (const newVar of colorVariations) {
+    const newSvg = svgMaster.cloneNode(true);
+    const newPaths = newSvg.querySelectorAll("path");
+
+    for (let i = 0; i < newPaths.length; ) {
+      if (newPaths[i].style.fill !== "none") {
+        newPaths[i].style.fill = newVar[i];
+        i++;
+      }
+      if (newPaths[i].style.stroke) {
+        newPaths[i].style.stroke = newVar[i];
+        i++;
+      }
+    }
+
+    // set svg background color
+    newSvg.style.backgroundColor = shirtColor;
+
+    results.appendChild(newSvg);
+  }
+}
+
+generateButton.addEventListener("click", makeVariations);
